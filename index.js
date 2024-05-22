fab5cae343432431b1b86015227a8e3fbaf9ebdf
Mohanterri <mohanterri0@gmail.com>
2024-05-22 08:01:04 +0100

    Save

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 require('rootpath')();
 const path = require('path');
+
+const { server } = require('./core/includes.js')
+const { Server } = require("socket.io");
+var p2pserver = require('socket.io-p2p-server').Server
+const io = new Server(server);
+io.use(p2pserver);
+
+io.on('connection', (socket) => {
+    socket.on('post', function(data) {
+        
+    });
+});
+
 module.exports = require('./core/includes.js');
